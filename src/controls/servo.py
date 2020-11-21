99e023b00e0f2b68b869272e2b4879ddf5c634ee
Jiří Mauritz <jirmauritz@gmail.com>
2020-11-21 17:01:48 +0100

    Setup ESC

--- src/controls/servo.py
+++ src/controls/servo.py
@@ -3,13 +3,14 @@ from time import sleep
 
 
 servoPIN = 17
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
-servo = GPIO.PWM(servoPIN, 50)
-servo.start(5.75)
+servo_pwm = GPIO.PWM(servoPIN, 50)
+servo_pwm.start(5.75)
 
 
 def steer(direction):
     # map < -100, 100 > into < 3, 8.5 > with 6 as center
     pwm = (direction / 36.4) + 5.75
-    servo.start(pwm)
+    servo_pwm.start(pwm)
