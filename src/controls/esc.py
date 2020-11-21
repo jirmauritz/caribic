079af0f8888f0d7e15634dcead4fe2248ef00869
Jiří Mauritz <jirmauritz@gmail.com>
2020-11-21 17:23:06 +0100

    ESC fix

--- src/controls/esc.py
+++ src/controls/esc.py
@@ -1,39 +1,39 @@
 import time   #importing time library to make Rpi wait because its too impatient
-import RPi.GPIO as GPIO
+import pigpio
 
 escPIN = 17  #Connect the ESC in this GPIO pin
 max_value = 2000
-mid_value = 1500
 min_value = 700
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(escPIN, GPIO.OUT)
-esc_pwm = GPIO.PWM(escPIN, 0)
+pi = pigpio.pi()
+pi.set_servo_pulsewidth(escPIN, 0)
 
 
 def speed(throttle):
     # 1100 - 1900
     pwm = (throttle * 4) + 1500
-    esc_pwm.start(pwm)
+    pi.set_servo_pulsewidth(escPIN, pwm)
 
-def calibrate():   #This is the auto calibration procedure of a normal ESC
+
+def calibrate():   #This is the auto calibration procedure of a normal escPIN
+    pi.set_servo_pulsewidth(escPIN, 0)
     print("Disconnect the battery and press Enter")
     inp = input()
     if inp == '':
-        esc_pwm.start(max_value)
+        pi.set_servo_pulsewidth(escPIN, max_value)
         print("Connect the battery NOW.. you will here two beeps, then wait for a gradual falling tone then press Enter")
         inp = input()
         if inp == '':            
-            esc_pwm.start(min_value)
+            pi.set_servo_pulsewidth(escPIN, min_value)
             print("Wierd eh! Special tone")
             time.sleep(7)
             print("Wait for it ....")
             time.sleep (5)
             print("Im working on it, DONT WORRY JUST WAIT.....")
-            esc_pwm.start(0)
+            pi.set_servo_pulsewidth(escPIN, 0)
             time.sleep(2)
-            print("Arming ESC now...")
-            esc_pwm.start(min_value)
+            print("Arming escPIN now...")
+            pi.set_servo_pulsewidth(escPIN, min_value)
             time.sleep(1)
             print("See.... uhhhhh")
 
