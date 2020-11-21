06aa746ead88ffcfacaba7f526af66f1e0d2ac59
Jiri Mauritz <jirmauritz@gmail.com>
2020-11-21 16:25:00 +0100

    Servo computations updated

--- src/controls/servo.py
+++ src/controls/servo.py
@@ -6,10 +6,10 @@ servoPIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 servo = GPIO.PWM(servoPIN, 50)
-servo.start(4.2)
+servo.start(5.75)
 
 
 def steer(direction):
-    # map < -100, 100 > into < 2.5, 5.9 >
-    pwm = (direction / 58.8) + 4.2
+    # map < -100, 100 > into < 3, 8.5 > with 6 as center
+    pwm = (direction / 36.4) + 5.75
     servo.start(pwm)
