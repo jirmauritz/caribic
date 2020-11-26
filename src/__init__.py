ade4ddcaf42fa0bb22411dd631573e198c05a048
Jiří Mauritz <jirmauritz@gmail.com>
2020-11-26 19:24:26 +0100

    Removed Flask-Bootstrap, intro BS4

--- src/__init__.py
+++ src/__init__.py
@@ -1,30 +1,11 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 
-from .frontend import frontend
-from .nav import nav
+from .frontend import frontend_blueprint
 
 
-def create_app(configfile=None):
-    # We are using the "Application Factory"-pattern here, which is described
-    # in detail inside the Flask docs:
-    # http://flask.pocoo.org/docs/patterns/appfactories/
+def create_app():
 
     app = Flask(__name__)
-
-    # Install our Bootstrap extension
-    Bootstrap(app)
-
-    # Our application uses blueprints as well; these go well with the
-    # application factory. We already imported the blueprint, now we just need
-    # to register it:
-    app.register_blueprint(frontend)
-
-    # Because we're security-conscious developers, we also hard-code disabling
-    # the CDN support (this might become a default in later versions):
-    app.config['BOOTSTRAP_SERVE_LOCAL'] = True
-
-    # We initialize the navigation as well
-    nav.init_app(app)
+    app.register_blueprint(frontend_blueprint)
 
     return app
