e113cb8294287132c2fbb4cc63960a5f18a2fca2
Jiří Mauritz <jirmauritz@gmail.com>
2020-11-30 00:08:00 +0100

    Stop Btn + non scroll + Disconnect alert

---
+++ src/static/caribic.js
@@ -0,0 +1,13 @@
+
+var connDiv = $('#connected')[0];
+var disconnDiv = $('#disconnected')[0];
+
+socket.on('connect', function() {
+        connDiv.style.display = "block";
+        disconnDiv.style.display = null;
+      })
+
+socket.on('disconnect', function() {
+        connDiv.style.display = null;
+        disconnDiv.style.display = "block";
+      })
--- src/static/throttle.js
+++ src/static/throttle.js
@@ -5,7 +5,7 @@ var th = {}
 var socket = io();
 
 th.xCenter = 50;
-th.yCenter = 150;
+th.yCenter = 100;
 th.stage = new createjs.Stage('throttle');
 
 th.psp = new createjs.Shape();
@@ -14,10 +14,10 @@ th.psp.graphics.beginFill('#333333').drawRect(th.xCenter-35, th.yCenter - 5, 70,
 th.psp.alpha = 0.5;
 
 th.vertical = new createjs.Shape();
-th.vertical.graphics.beginFill('#ff4d4d').drawRect(50, 0, 2, 300);
+th.vertical.graphics.beginFill('#ff4d4d').drawRect(50, 0, 2, 200);
 
 th.horizontal = new createjs.Shape();
-th.horizontal.graphics.beginFill('#ff4d4d').drawRect(th.xCenter-50, th.yCenter, 300, 2);
+th.horizontal.graphics.beginFill('#ff4d4d').drawRect(th.xCenter-50, th.yCenter, 200, 2);
 
 th.stage.addChild(th.psp);
 th.stage.addChild(th.vertical);
@@ -37,9 +37,9 @@ th.hm = new Hammer.Manager(throttle, {
 
 var update_throttle = function(ev) {
         var bb = ev.target.getBoundingClientRect();
-        y = Math.min(Math.max(ev.center.y - bb.top - th.yCenter, -145), 145);
+        y = Math.min(Math.max(ev.center.y - bb.top - th.yCenter, -95), 95);
         if (th.psp.y != y) {
-            value = Math.round( -100 * y / 145 );
+            value = Math.round( -100 * y / 95 );
             socket.emit('throttle', value);
             $('#tVal').text('Throttle: ' + value);
             th.psp.y = y;
@@ -59,3 +59,11 @@ th.hm.on("panend", function(ev) {
         th.psp.alpha = 0.5;
         th.stage.update();
     });
+
+var stopBtn = $('#stopBtn')[0];
+stopBtn.onclick = function() {
+    socket.emit('throttle', 0);
+    $('#tVal').text('Throttle: 0');
+    th.psp.y = 0;
+    th.stage.update();
+};
