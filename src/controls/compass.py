11c79a6aeac4f1776ec514d2c7337b1d662f8973
Jiri Mauritz <jirmauritz@gmail.com>
2021-04-13 22:36:04 +0200

    Compass controls

--- src/controls/compass.py
+++ src/controls/compass.py
@@ -1,29 +1,39 @@
 import FaBo9Axis_MPU9250
 import time
 import sys
+from math import atan2, pi, sqrt
 
-mpu9250 = FaBo9Axis_MPU9250.MPU9250()
+LSB = 0.48828125
+R2D = 180.0 / pi
 
-try:
-    while True:
+minx = 100
+maxx = -100
+
+
+# hx = 0 - 90
+# hy = -16 - 43
+# hz = -107 - -35
+
+while True:
+    try:
+        mpu9250 = FaBo9Axis_MPU9250.MPU9250()
         accel = mpu9250.readAccel()
-        print(" ax = " , ( accel['x'] ))
-        print(" ay = " , ( accel['y'] ))
-        print(" az = " , ( accel['z'] ))
-
-        gyro = mpu9250.readGyro()
-        print(" gx = " , ( gyro['x'] ))
-        print(" gy = " , ( gyro['y'] ))
-        print(" gz = " , ( gyro['z'] ))
-
         mag = mpu9250.readMagnet()
-        print(" mx = " , ( mag['x'] ))
-        print(" my = " , ( mag['y'] ))
-        print(" mz = " , ( mag['z'] ))
-        print()
-
-        time.sleep(0.1)
-
-except KeyboardInterrupt:
-    sys.exit()
+        D = None
+        hx = mag['x'] - 45
+        hy = mag['y'] - 29.5
+        hz = mag['z'] + 71
+ #       h = sqrt(hx * hx + hy * hy + hz * hz)
+ #       hx /= h
+ #       hy /= h
+ #       hz /= h
+        yaw_rad = atan2(hy, hx)
+        heading_rad = yaw_rad *R2D
+        D = heading_rad % 360
+        print(D)
+        time.sleep(0.2)
+    except OSError:
+        print('Error')
+        time.sleep(0.5)
+        pass
 
