fb6fc0168ca250f5ac374cc8a98dab72cf4480f9
Jiří Mauritz <jirmauritz@gmail.com>
2021-04-14 22:48:06 +0200

    GPS working: marker + js + tiles

--- src/static/js/gps.js
+++ src/static/js/gps.js
@@ -1,10 +1,28 @@
-var map = L.map('mapid').setView([50.0996211, 14.581498], 19);
+// init sockerIO
+var socket = io();
 
+var map = L.map('mapid').setView([50.0996211, 14.581498], 18);
 
-var offlineLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    subdomains: 'abc',
-    minZoom: 19,
+var boatIcon = L.icon({
+    iconUrl: 'static/img/marker_boat.png',
+    iconSize:     [10, 30], // size of the icon
+    iconAnchor:   [5, 15], // point of the icon which will correspond to marker's location
+});
+
+var offlineLayer = L.tileLayer('/static/tiles/{z}/{x}/{y}.png', {
+    minZoom: 18,
     maxZoom: 19,
-    crossOrigin: false
+    maxNativeZoom:18,
+    iconUrl: 'img/marker_boat.png'
 }).addTo(map);
+
+var marker = L.marker([50.0996211, 14.581498], {icon: boatIcon}).addTo(map);
+
+var btn = $('#btn')[0];
+btn.onclick = function() {
+    socket.emit('btn', 0);
+};
+
+socket.on("location", (satellites, lat, lng) => {
+  marker.setLatLng(new L.LatLng(lat, lng));
+});
